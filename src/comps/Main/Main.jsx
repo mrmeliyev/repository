0b8a136e3c0c618f2d8183ef292b1e7c9c60f91d
VicN1 <1aziz1xon1@gmail.com>
2026-01-18 18:12:52 +0500

    hey

--- src/comps/Main/Main.jsx
+++ src/comps/Main/Main.jsx
@@ -8,7 +8,6 @@ import Section5 from '../Section5/Section5'
 function Main() {
     return (
         <main>
-            <h1>Hello World</h1>
             <Section1 />
             <Section2 />
             <Section3 />
